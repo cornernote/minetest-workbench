57c5ebce5019668abbf671b15c9223bcf42ebd24
darkrose <lisa@ltmnet.com>
2012-07-23 16:42:19 +1000

    update workbench to work with new formspec

--- workbench/init.lua
+++ workbench/init.lua
@@ -3,17 +3,17 @@
 -- Copyright (C) Lisa Milne 2012 <lisa@ltmnet.com>
 --
 -- This program is free software: you can redistribute it and/or modify
--- it under the terms of the GNU Lesser General Public License as 
--- published by the Free Software Foundation, either version 2 of the 
+-- it under the terms of the GNU Lesser General Public License as
+-- published by the Free Software Foundation, either version 2 of the
 -- License, or (at your option) any later version.
 --
 -- This program is distributed in the hope that it will be useful, but
 -- WITHOUT ANY WARRANTY; without even the implied warranty of
--- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU 
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
 -- Lesser General Public License for more details.
 --
--- You should have received a copy of the GNU Lesser General Public 
--- License along with this program.  If not, see 
+-- You should have received a copy of the GNU Lesser General Public
+-- License along with this program.  If not, see
 -- <http://www.gnu.org/licenses/>
 
 minetest.register_node("workbench:3x3", {
@@ -26,7 +26,7 @@ minetest.register_node("workbench:3x3", {
 	on_construct = function(pos)
 		local meta = minetest.env:get_meta(pos)
 		meta:set_string("formspec",
-			"invsize[8,9;]"..
+			"size[8,9]"..
 			"list[current_name;table;1,1;3,3;]"..
 			"list[current_name;dst;6,2;1,1;]"..
 			"list[current_player;main;0,5;8,4;]")
@@ -52,11 +52,11 @@ minetest.register_node("workbench:3x3", {
 			local inv = meta:get_inventory()
 			local tablelist = inv:get_list("table")
 			local crafted = nil
-		
+
 			if tablelist then
 				crafted = minetest.get_craft_result({method = "normal", width = 3, items = tablelist})
 			end
-			
+
 			if crafted then
 				inv:set_stack("dst", 1, crafted.item)
 			else
@@ -75,11 +75,11 @@ minetest.register_node("workbench:3x3", {
 			local inv = meta:get_inventory()
 			local tablelist = inv:get_list("table")
 			local crafted = nil
-		
+
 			if tablelist then
 				crafted = minetest.get_craft_result({method = "normal", width = 3, items = tablelist})
 			end
-			
+
 			if crafted then
 				inv:set_stack("dst", 1, crafted.item)
 			else
@@ -96,11 +96,11 @@ minetest.register_node("workbench:3x3", {
 			local inv = meta:get_inventory()
 			local tablelist = inv:get_list("table")
 			local crafted = nil
-		
+
 			if tablelist then
 				crafted = minetest.get_craft_result({method = "normal", width = 3, items = tablelist})
 			end
-			
+
 			if crafted then
 				inv:set_stack("dst", 1, crafted.item)
 			else
@@ -112,11 +112,11 @@ minetest.register_node("workbench:3x3", {
 			local tablelist = inv:get_list("table")
 			local crafted = nil
 			local table_dec = nil
-		
+
 			if tablelist then
 				crafted,table_dec = minetest.get_craft_result({method = "normal", width = 3, items = tablelist})
 			end
-			
+
 			if table_dec then
 				inv:set_list("table", table_dec.items)
 			else
@@ -124,11 +124,11 @@ minetest.register_node("workbench:3x3", {
 			end
 
 			local tablelist = inv:get_list("table")
-		
+
 			if tablelist then
 				crafted,table_dec = minetest.get_craft_result({method = "normal", width = 3, items = tablelist})
 			end
-			
+
 			if crafted then
 				inv:set_stack("dst", 1, crafted.item)
 			else
@@ -149,7 +149,7 @@ minetest.register_node("workbench:4x4", {
 	on_construct = function(pos)
 		local meta = minetest.env:get_meta(pos)
 		meta:set_string("formspec",
-			"invsize[8,9;]"..
+			"size[8,9]"..
 			"list[current_name;table;1,0;4,4;]"..
 			"list[current_name;dst;6,2;1,1;]"..
 			"list[current_player;main;0,5;8,4;]")
@@ -175,11 +175,11 @@ minetest.register_node("workbench:4x4", {
 			local inv = meta:get_inventory()
 			local tablelist = inv:get_list("table")
 			local crafted = nil
-		
+
 			if tablelist then
 				crafted = minetest.get_craft_result({method = "normal", width = 4, items = tablelist})
 			end
-			
+
 			if crafted then
 				inv:set_stack("dst", 1, crafted.item)
 			else
@@ -198,11 +198,11 @@ minetest.register_node("workbench:4x4", {
 			local inv = meta:get_inventory()
 			local tablelist = inv:get_list("table")
 			local crafted = nil
-		
+
 			if tablelist then
 				crafted = minetest.get_craft_result({method = "normal", width = 4, items = tablelist})
 			end
-			
+
 			if crafted then
 				inv:set_stack("dst", 1, crafted.item)
 			else
@@ -219,11 +219,11 @@ minetest.register_node("workbench:4x4", {
 			local inv = meta:get_inventory()
 			local tablelist = inv:get_list("table")
 			local crafted = nil
-		
+
 			if tablelist then
 				crafted = minetest.get_craft_result({method = "normal", width = 4, items = tablelist})
 			end
-			
+
 			if crafted then
 				inv:set_stack("dst", 1, crafted.item)
 			else
@@ -235,11 +235,11 @@ minetest.register_node("workbench:4x4", {
 			local tablelist = inv:get_list("table")
 			local crafted = nil
 			local table_dec = nil
-		
+
 			if tablelist then
 				crafted,table_dec = minetest.get_craft_result({method = "normal", width = 4, items = tablelist})
 			end
-			
+
 			if table_dec then
 				inv:set_list("table", table_dec.items)
 			else
@@ -247,11 +247,11 @@ minetest.register_node("workbench:4x4", {
 			end
 
 			local tablelist = inv:get_list("table")
-		
+
 			if tablelist then
 				crafted,table_dec = minetest.get_craft_result({method = "normal", width = 4, items = tablelist})
 			end
-			
+
 			if crafted then
 				inv:set_stack("dst", 1, crafted.item)
 			else
@@ -272,7 +272,7 @@ minetest.register_node("workbench:5x5", {
 	on_construct = function(pos)
 		local meta = minetest.env:get_meta(pos)
 		meta:set_string("formspec",
-			"invsize[8,10;]"..
+			"size[8,10]"..
 			"list[current_name;table;0,0;5,5;]"..
 			"list[current_name;dst;6,2;1,1;]"..
 			"list[current_player;main;0,6;8,4;]")
@@ -298,11 +298,11 @@ minetest.register_node("workbench:5x5", {
 			local inv = meta:get_inventory()
 			local tablelist = inv:get_list("table")
 			local crafted = nil
-		
+
 			if tablelist then
 				crafted = minetest.get_craft_result({method = "normal", width = 5, items = tablelist})
 			end
-			
+
 			if crafted then
 				inv:set_stack("dst", 1, crafted.item)
 			else
@@ -321,11 +321,11 @@ minetest.register_node("workbench:5x5", {
 			local inv = meta:get_inventory()
 			local tablelist = inv:get_list("table")
 			local crafted = nil
-		
+
 			if tablelist then
 				crafted = minetest.get_craft_result({method = "normal", width = 5, items = tablelist})
 			end
-			
+
 			if crafted then
 				inv:set_stack("dst", 1, crafted.item)
 			else
@@ -342,11 +342,11 @@ minetest.register_node("workbench:5x5", {
 			local inv = meta:get_inventory()
 			local tablelist = inv:get_list("table")
 			local crafted = nil
-		
+
 			if tablelist then
 				crafted = minetest.get_craft_result({method = "normal", width = 5, items = tablelist})
 			end
-			
+
 			if crafted then
 				inv:set_stack("dst", 1, crafted.item)
 			else
@@ -358,11 +358,11 @@ minetest.register_node("workbench:5x5", {
 			local tablelist = inv:get_list("table")
 			local crafted = nil
 			local table_dec = nil
-		
+
 			if tablelist then
 				crafted,table_dec = minetest.get_craft_result({method = "normal", width = 5, items = tablelist})
 			end
-			
+
 			if table_dec then
 				inv:set_list("table", table_dec.items)
 			else
@@ -370,11 +370,11 @@ minetest.register_node("workbench:5x5", {
 			end
 
 			local tablelist = inv:get_list("table")
-		
+
 			if tablelist then
 				crafted,table_dec = minetest.get_craft_result({method = "normal", width = 5, items = tablelist})
 			end
-			
+
 			if crafted then
 				inv:set_stack("dst", 1, crafted.item)
 			else
